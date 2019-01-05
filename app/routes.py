efbe1a89c10ef2b764c94f6e02410679ad271954
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-05 21:50:07 +0530

    home page- mock user using templates

--- app/routes.py
+++ app/routes.py
@@ -5,22 +5,11 @@ Created on Sat Jan  5 21:20:22 2019
 
 @author: mudita
 """
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     user= {'username': 'utkarsh'}
-    return '''
-    <html>
-    <head>
-    <title> Homepage- Microblog</title>
-    </head>
-    <body>
-    <h1> Hello, ''' + user['username'] +''' !</h1>
-    </body>
-    </html>
-    
-    
-    
-    '''
+    return render_template('index.html', title='Home', user=user)
