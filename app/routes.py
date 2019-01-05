deef464678a06adce4f816e09b3898493fe10d1a
WEIRDLEMON <utkarshdhy@gmail.com>
2019-01-05 22:02:37 +0530

    home page- mock user using templates-conditionals

--- app/routes.py
+++ app/routes.py
@@ -12,4 +12,14 @@ from app import app
 @app.route('/index')
 def index():
     user= {'username': 'utkarsh'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
     return render_template('index.html', title='Home', user=user)
